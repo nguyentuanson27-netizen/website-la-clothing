ffd5d1b6a64e8466031d78490ffc9665317f1310
nguyentuanson27-netizen <nguyentuanson27@gmail.com>
2026-08-09 22:25:47 +0700

    db: add Better Auth lookup indexes

---
+++ prisma/migrations/20260809152500_add_auth_lookup_indexes/migration.sql
@@ -0,0 +1,3 @@
+CREATE INDEX "session_userId_idx" ON "session"("userId");
+CREATE INDEX "account_userId_idx" ON "account"("userId");
+CREATE INDEX "verification_identifier_idx" ON "verification"("identifier");
