e62138e1c914f09c3b5225c1e40ac8da37c28f43
nguyentuanson27-netizen <nguyentuanson27@gmail.com>
2026-08-22 09:27:11 +0700

    refactor(p16): keep robots route as thin adapter

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,35 +1,8 @@
 import type { MetadataRoute } from "next";
 import { connection } from "next/server";
 
-import {
-  CRAWL_BLOCKED_PATHS,
-  readSearchExposure,
-  type SearchExposure,
-} from "@/seo/search-exposure";
-
-export function buildRobotsDocument(exposure: SearchExposure): MetadataRoute.Robots {
-  const rules: MetadataRoute.Robots["rules"] = [
-    {
-      userAgent: "*",
-      allow: "/",
-      disallow: [...CRAWL_BLOCKED_PATHS],
-    },
-    {
-      userAgent: "OAI-SearchBot",
-      allow: "/",
-      disallow: [...CRAWL_BLOCKED_PATHS],
-    },
-  ];
-
-  if (!exposure.indexingEnabled) {
-    return { rules };
-  }
-
-  return {
-    rules,
-    sitemap: new URL("/sitemap.xml", exposure.origin).href,
-  };
-}
+import { buildRobotsDocument } from "@/seo/robots-policy";
+import { readSearchExposure } from "@/seo/search-exposure";
 
 export default async function robots(): Promise<MetadataRoute.Robots> {
   await connection();
