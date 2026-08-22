e747f7d8919053298bddc30d2f1bd438ff1d0530
nguyentuanson27-netizen <nguyentuanson27@gmail.com>
2026-08-22 09:23:11 +0700

    feat(p16): preserve public OAI search crawl access

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,16 +1,25 @@
 import type { MetadataRoute } from "next";
 import { connection } from "next/server";
 
-import { CRAWL_BLOCKED_PATHS, readSearchExposure } from "@/seo/search-exposure";
+import {
+  CRAWL_BLOCKED_PATHS,
+  readSearchExposure,
+  type SearchExposure,
+} from "@/seo/search-exposure";
 
-export default async function robots(): Promise<MetadataRoute.Robots> {
-  await connection();
-  const exposure = readSearchExposure();
-  const rules = {
-    userAgent: "*",
-    allow: "/",
-    disallow: [...CRAWL_BLOCKED_PATHS],
-  };
+export function buildRobotsDocument(exposure: SearchExposure): MetadataRoute.Robots {
+  const rules: MetadataRoute.Robots["rules"] = [
+    {
+      userAgent: "*",
+      allow: "/",
+      disallow: [...CRAWL_BLOCKED_PATHS],
+    },
+    {
+      userAgent: "OAI-SearchBot",
+      allow: "/",
+      disallow: [...CRAWL_BLOCKED_PATHS],
+    },
+  ];
 
   if (!exposure.indexingEnabled) {
     return { rules };
@@ -20,4 +29,9 @@ export default async function robots(): Promise<MetadataRoute.Robots> {
     rules,
     sitemap: new URL("/sitemap.xml", exposure.origin).href,
   };
+}
+
+export default async function robots(): Promise<MetadataRoute.Robots> {
+  await connection();
+  return buildRobotsDocument(readSearchExposure());
 }
